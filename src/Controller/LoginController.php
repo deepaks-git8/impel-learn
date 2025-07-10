d91a22d1107768688ab1d78ff2ce7951e59d7a3b
Deepak Kumar <deepakkumar.cse2@gmail.com>
2025-07-11 01:35:54 +0530

    Implemented user login, role-based access, and DB migration

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,18 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(): Response
+    public function index(AuthenticationUtils $utils): Response
     {
-//        just checking
+        $lastUsername = $utils->getLastUsername();
+        $error = $utils->getLastAuthenticationError();
         return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
+            'lastUsername' => $lastUsername,
+            'error' => $error
         ]);
     }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(){
+
+    }
+    #[Route('/', name: 'app_home')]
+    public function home(): Response
+    {
+        return $this->render('base.html.twig');
+    }
+
 }
